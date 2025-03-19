ccf39c569ce4e0b2f00fb731907c00e79b8b16ef
Martahdzz <118076716+Martahdzz@users.noreply.github.com>
2025-03-19 09:45:31 +0100

    ubuntu

--- Practica2/src/config.js
+++ Practica2/src/config.js
@@ -1,12 +1,16 @@
 import { join, dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const config = {
     port: 3000,
-    recursos: join(dirname(import.meta.dirname), 'static'),
-    vistas: join(dirname(import.meta.dirname), 'vistas'),
+    recursos: join(__dirname, '../static'),
+    vistas: join(__dirname, '../vistas'), // Ajusta la ruta para apuntar al directorio correcto
     session: {
         resave: false,
         saveUninitialized: true,
         secret: 'no muy secreto'
     }
-}
+};
