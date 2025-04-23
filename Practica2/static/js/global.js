5109a504d4ff69a98508ab8527649384a8672314
alalba03 <alalba03@ucm.es>
2025-04-23 10:17:23 +0200

    archivo TODO

--- Practica2/static/js/global.js
+++ Practica2/static/js/global.js
@@ -1,15 +1,20 @@
+if(document.querySelector('.welcome-popup')) {
 
-if (!sessionStorage.getItem('welcomePopupShown')) {
+    if(!sessionStorage.getItem('welcomePopupShown')) {
 
-    const popup = document.querySelector('.welcome-popup');
-    popup.style.display = 'block';
-
-    setTimeout(() => {
-        popup.style.opacity = '0';
+        // Show the popup if it hasn't been shown before
+        const popup = document.querySelector('.welcome-popup');
+        popup.style.opacity = '1';
+        popup.style.display = 'block';
+        
         setTimeout(() => {
-            popup.style.display = 'none';
-        }, 500);
-    }, 3000);
+            popup.style.opacity = '0';
+            setTimeout(() => {
+                popup.style.display = 'none';
+            }, 500);
+        }, 3000);
+        
+        sessionStorage.setItem('welcomePopupShown', 'true');
+    }
 
-    sessionStorage.setItem('welcomePopupShown', 'true');
 }
