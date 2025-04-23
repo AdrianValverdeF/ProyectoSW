2e905930633497d4cfac331d8213d456068ae016
alalba03 <alalba03@ucm.es>
2025-04-23 09:47:39 +0200

    cambios a css de botones y margin de eventos

--- Practica2/static/js/global.js
+++ Practica2/static/js/global.js
@@ -1,6 +1,15 @@
-setTimeout(() => {
-    document.querySelector('.welcome-popup').style.opacity = '0';
+
+if (!sessionStorage.getItem('welcomePopupShown')) {
+
+    const popup = document.querySelector('.welcome-popup');
+    popup.style.display = 'block';
+
     setTimeout(() => {
-        document.querySelector('.welcome-popup').style.display = 'none';
-    }, 500);
-}, 3000);
+        popup.style.opacity = '0';
+        setTimeout(() => {
+            popup.style.display = 'none';
+        }, 500);
+    }, 3000);
+
+    sessionStorage.setItem('welcomePopupShown', 'true');
+}
