c6e4aec71da6da1404f52a2e88aaf1f96ba40f54
alalba03 <alalba03@ucm.es>
2025-04-25 00:23:21 +0200

    cartera en cabecera (mejorar css) + bienvenida de usuario comentada (propongo quitarla)

--- Practica2/static/js/global.js
+++ Practica2/static/js/global.js
@@ -1,20 +1,20 @@
-if(document.querySelector('.welcome-popup')) {
+// if(document.querySelector('.welcome-popup')) {
 
-    if(!sessionStorage.getItem('welcomePopupShown')) {
+//     if(!sessionStorage.getItem('welcomePopupShown')) {
 
-        // Show the popup if it hasn't been shown before
-        const popup = document.querySelector('.welcome-popup');
-        popup.style.opacity = '1';
-        popup.style.display = 'block';
+//         // Show the popup if it hasn't been shown before
+//         const popup = document.querySelector('.welcome-popup');
+//         popup.style.opacity = '1';
+//         popup.style.display = 'block';
         
-        setTimeout(() => {
-            popup.style.opacity = '0';
-            setTimeout(() => {
-                popup.style.display = 'none';
-            }, 500);
-        }, 3000);
+//         setTimeout(() => {
+//             popup.style.opacity = '0';
+//             setTimeout(() => {
+//                 popup.style.display = 'none';
+//             }, 500);
+//         }, 3000);
         
-        sessionStorage.setItem('welcomePopupShown', 'true');
-    }
+//         sessionStorage.setItem('welcomePopupShown', 'true');
+//     }
 
-}
+// }
